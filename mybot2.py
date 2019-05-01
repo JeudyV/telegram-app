e4c163189c8c78be72a8d0278a636176bc74179b
JeudyV <jeu09cr@hotmail.com>
2019-04-30 23:57:48 -0600

    update funtion getTextTwit

--- mybot2.py
+++ mybot2.py
@@ -2,9 +2,7 @@ import telegram
 from telegram.ext import *
 from bs4 import BeautifulSoup
 import requests
-
-#mi_bot = telegram.Bot("768133708:AAGpMhnQFkJmEP5VzR9Cs5MgXXd8ZQy21Jc")
-#mi_bot_updater = Updater(mi_bot.token)
+import csv
 
 
 def getInfoFllw(profile):
@@ -14,13 +12,14 @@ def getInfoFllw(profile):
         follow_box = bs.find('li', {'class': 'ProfileNav-item ProfileNav-item--followers'})
         followers = follow_box.find('a').find('span', {'class': 'ProfileNav-value'})
         fllw = followers.get('data-count')
-        result = "Followers for user " + profile + ": " + fllw + " users"
-        return result
+        #result = "Followers for user " + profile + ": " + fllw + " users"
+        return fllw
     except:
         print("Exception in user code:")
         print('-' * 60) 
         traceback.print_exc(file=sys.stdout)
         print('-' * 60)
+
 
 def getInfoTwi(profile):
     temp = requests.get('https://twitter.com/' + profile)
@@ -28,18 +27,75 @@ def getInfoTwi(profile):
     try:
         follow_box = bs.find('li', {'class': 'ProfileNav-item ProfileNav-item--tweets is-active'})
         followers = follow_box.find('a').find('span', {'class': 'ProfileNav-value'})
-        fllw = followers.get('data-count')
-        result = "Followers for user " + profile + ": " + fllw + " users"
-        return result
+        Twit = followers.get('data-count')
+        #result = "Followers for user " + profile + ": " + fllw + " users"
+        return Twit
     except:
         print("Exception in user code:")
         print('-' * 60)
         traceback.print_exc(file=sys.stdout)
         print('-' * 60)
 
-def listenerFllw(bot, update, args):
+
+def getTextTwit(profile):
+    array = []
+    temp = requests.get('https://twitter.com/' + profile)
+    bs = BeautifulSoup(temp.text, 'lxml')
+    try:
+        twit_box = bs.find_all('li', {'class': 'js-stream-item stream-item stream-item'})
+        for box in range(len(twit_box)-15):
+            txt = twit_box[box].find('p', {'class': 'TweetTextSize TweetTextSize--normal '
+                                                    'js-tweet-text tweet-text'}).text
+            array.append(txt)
+        return array
+    except:
+        print("Exception in user code:")
+        print('-' * 60)
+        traceback.print_exc(file=sys.stdout)
+        print('-' * 60)
+
+
+def listener_twit(bot, update, args):
+    prof = listener(bot, update, args)
+    twit_text = getTextTwit(prof)
+    count_twit = getInfoTwi(prof)
+    message = "Tweets for user " + prof + ": " + count_twit + " Tweet"
+    bot.sendMessage(chat_id=update.message.chat_id, text=message)
+    for x in twit_text:
+        bot.sendMessage(chat_id=update.message.chat_id, text=x)
+
+
+def listener(bot, update, args):
     bot.sendMessage(chat_id=update.message.chat_id, text="Reading!")
-    #profile = str(update.message.text)
+    profile = str(args[0])
+    if "twitter.com" in profile:
+        message2 = profile.split("/")
+        for i in range(len(message2)):
+            if i+1 == len(message2):
+                profile = message2[i]
+    return profile
+
+
+def listener_fllow(bot, update, args):
+    prof = listener(bot, update, args)
+    result = getInfoFllw(prof)
+    bot.sendMessage(chat_id=update.message.chat_id, text=("total amount of followers: ",result))
+    twit_file = csv.writer(open("followers_file.csv", "w"))
+    twit_file.writerow(["Profile", "followers"])
+    twit_file.writerow([prof, result])
+
+
+def listenerT(bot, update, args):
+    prof = listener(bot, update, args)
+    result = getInfoTwi(prof)
+    bot.sendMessage(chat_id=update.message.chat_id, text=("total amount of tweets: ", result))
+    twit_file = csv.writer(open("tweets_file.csv", "w"))
+    twit_file.writerow(["Profile", "tweets"])
+    twit_file.writerow([prof, result])
+
+
+"""def listenerFllw(bot, update, args):
+    bot.sendMessage(chat_id=update.message.chat_id, text="Reading!")
     profile = str(args[0])
     print("param", profile)
     if "twitter.com" in profile:
@@ -49,11 +105,11 @@ def listenerFllw(bot, update, args):
                 profile = message2[i]
     print(profile)
     result = getInfoFllw(profile)
-    bot.sendMessage(chat_id=update.message.chat_id, text=result)
+    bot.sendMessage(chat_id=update.message.chat_id, text=result)"""
 
-def listenerTwi(bot, update, args):
+
+"""def listenerTwi(bot, update, args):
     bot.sendMessage(chat_id=update.message.chat_id, text="Reading!")
-    #profile = str(update.message.text)
     profile = str(args[0])
     print("param", profile)
     if "twitter.com" in profile:
@@ -62,39 +118,36 @@ def listenerTwi(bot, update, args):
             if i+1 == len(message2):
                 profile = message2[i]
     print(profile)
-    result = getInfoTwi(profile)
+    result = str(getInfoTwi(profile))
+    print(result)
     bot.sendMessage(chat_id=update.message.chat_id, text=result)
+    twit_file = csv.writer(open("tweets_file.csv", "w"))
+    twit_file.writerow(["Profile", "tweets"])
+    twit_file.writerow([profile, result])"""
 
-#listener_handler = MessageHandler(Filters.text, listener)
-#variable = CommandHandler("tweets", listenerTwi)
-#mi_bot_updater.dispatcher.add_handler(listener_handler)
-
-#mi_bot_updater.start_polling()
-#mi_bot_updater.idle()
 
 def main(bot_token):
-  """ Main function of the bot """
-  updater = Updater(token=bot_token)
-  dispatcher = updater.dispatcher
+    """ Main function of the bot """
+    updater = Updater(token=bot_token)
+    dispatcher = updater.dispatcher
 
-  # Other handlers
-  #listener_handler = MessageHandler(Filters.text, listenerTwi)
-  add_handler_Fllw = CommandHandler('followers', listenerFllw, pass_args=True)
-  add_handler_Twi = CommandHandler('tweets', listenerTwi, pass_args=True)
+    # Other handlers
+    #listener_handler = MessageHandler(Filters.text, listenerTwi)
+    add_handler_Fllw = CommandHandler('followers', listener_fllow, pass_args=True)
+    add_handler_Twi = CommandHandler('tweets', listener_twit, pass_args=True)
 
-  # Add the handlers to the bot
-  #dispatcher.add_handler(listener_handler)
-  dispatcher.add_handler(add_handler_Fllw)
-  dispatcher.add_handler(add_handler_Twi)
+    # Add the handlers to the bot
+    dispatcher.add_handler(add_handler_Fllw)
+    dispatcher.add_handler(add_handler_Twi)
 
+    # Starting the bot
+    updater.start_polling()
+    updater.idle()
 
-  # Starting the bot
-  updater.start_polling()
-  updater.idle()
 
 if __name__ == "__main__":
-  TOKEN = "768133708:AAGpMhnQFkJmEP5VzR9Cs5MgXXd8ZQy21Jc"
-  main(TOKEN)
+    TOKEN = "768133708:AAGpMhnQFkJmEP5VzR9Cs5MgXXd8ZQy21Jc"
+    main(TOKEN)
 
 while True:
     pass
